fa31a45342bbebc7b65093069b48cab81787530d
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-05 00:28:16 +0400

    Решение задачи два

--- HomeWork/Task4/Program.cs
+++ HomeWork/Task4/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+using static System.Console;
+
+int max = 0;
+
+Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(ReadLine());
+
+Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(ReadLine());
+
+Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+WriteLine("max = " + max);
