16032aa00332fd245b99f1b1575fa72f7c720e82
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-05 00:59:43 +0400

    Решение задачи 4

--- HomeWork/Task8/Program.cs
+++ HomeWork/Task8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+using static System.Console;
+
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+
+int a = 2;
+
+if(number > 1)
+{
+    while(a <= number)
+    {
+        Write(a + " ");
+        a = a + 2;
+    }
+}
