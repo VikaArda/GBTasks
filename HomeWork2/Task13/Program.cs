e465cf15dc3024bea8c7d972981d02e8b56c16ce
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-09 01:02:57 +0400

    Решение задачи 13

--- HomeWork2/Task13/Program.cs
+++ HomeWork2/Task13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 6
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a>99) 
+{
+    int a1 = a/100;
+    int a2 = a1%10;
+    WriteLine(a2);
+}
+else 
+{
+    WriteLine("Третьей цифры нет");
+}
+
+
