22643201c2a5534aa5e61f85315252583e80ebd2
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-09 01:32:23 +0400

    Решение задачи 15

--- HomeWork2/Task15норм/Program.cs
+++ HomeWork2/Task15норм/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+using static System.Console;
+
+Write("Введите число: ");
+int day = Convert.ToInt32(ReadLine());
+
+if (day <=7 && day>=1)
+{
+    WriteLine(day==6|day==7?"Выходной":"Рабочий день");
+}
+else 
+{
+    WriteLine("Этого дня недели не существует");
+}
