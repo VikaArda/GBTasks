7d0037c6eccb0da76f25dfd6d7fe3f5e10a46794
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-08 23:53:11 +0400

    Решение задачи 10

--- HomeWork2/Task10/Program.cs
+++ HomeWork2/Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+using static System.Console;
+
+Clear();
+int number = new Random().Next(100,1000);
+WriteLine(number);
+int a1 = number/10;
+int a2 = a1%10;
+WriteLine(a2);
+
+
+
