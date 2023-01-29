3d1b5223ae9181dd31e9a28c6a4c031f1472f314
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-01-29 16:32:03 +0400

    Решение задачи 25

--- HomeWork4/Task25/Program.cs
+++ HomeWork4/Task25/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// В задаче не использвать стандартный метод Pow!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+using static System.Console;
+Clear();
+
+Write("Введите превое число a: ");
+int.TryParse(ReadLine(), out int a);
+Write("Введите число b: ");
+int.TryParse(ReadLine(), out int b);
+
+WriteLine($"{a} в {b} степени = {VozvStep(a,b)}");
+
+
+int VozvStep (int A,int B)
+{
+    if (A == 0) return 0;
+    int result = 1;
+    for (int i=0; i<B; i++)
+    result *= A;
+    return result;
+}
