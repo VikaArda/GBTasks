449f1e2789f426e499e231d55c1ed27cec95730a
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-02-13 17:31:57 +0400

    Решение задачи 47

--- HomeWork7/Task47/Program.cs
+++ HomeWork7/Task47/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу реализующую метод, который возвращает массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите M: ");
+int M = int.Parse(ReadLine()!);
+WriteLine("Введите N: ");
+int N = int.Parse(ReadLine()!);
+
+int [,] ar = new int [M, N];
+FullArray(ar);
+PrintArray(ar);
+
+void FullArray (int [,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray (int [,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+    WriteLine();
+    }
+}
+
