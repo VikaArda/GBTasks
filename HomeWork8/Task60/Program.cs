99e99ffb0256cb557a9c304ed81341e71b12c2ab
Vika Arda <vichka.ardasheva.00@mail.ru>
2023-02-16 20:30:31 +0400

    Решение задачи 60

--- HomeWork8/Task60/Program.cs
+++ HomeWork8/Task60/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+
+using static System.Console;
+Clear();
+
+int[,,] Array = GetArray(2, 2, 2, 1, 100);
+PrintArray(Array);
+WriteLine();
+
+
+int[,,] GetArray(int pows, int columns, int depth, int minValue, int maxValue)
+{
+    int[,,] result = new int[pows, columns, depth];
+    int[] temp = new int[result.GetLength(0) * result.GetLength(1) * result.GetLength(2)];
+    int number = 0;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < result.GetLength(0); x++)
+    {
+        for (int y = 0; y < result.GetLength(1); y++)
+        {
+            for (int z = 0; z < result.GetLength(2); z++)
+            {
+                result[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Write($"{inArray[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+        WriteLine();
+    }
+}
